6180ef91f8c1753f42efa5241fffbcc1f10fcff8
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-12 17:06:25 +0000

    Deploying to gh-pages from @ OlhaBuhun/Js_homework__9@008935b25f3a04cb3f3f51cad3b483beea541f69 🚀

--- 01-color-switcher.66d8f753.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){console.log(t.currentTarget),body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`})),o.addEventListener("click",(function(){}));
-//# sourceMappingURL=01-color-switcher.66d8f753.js.map
