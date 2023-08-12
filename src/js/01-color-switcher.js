008935b25f3a04cb3f3f51cad3b483beea541f69
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-12 20:05:56 +0300

    Wrote a script for 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,27 @@
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
 startButton.addEventListener('click', onStart);
 stopButton.addEventListener('click', onStop);
 
-function onStart(evt){
-  console.log(evt.currentTarget);
-  body.style.backgroundColor =  getRandomHexColor();
+let timerId = null;
+
+
+function onStart(){
+  startButton.setAttribute('disabled', true);
+  stopButton.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor =  getRandomHexColor();
+  }, 1000)
+  
 };
 
-function onStop(){};
+function onStop(){
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+  clearInterval(timerId);
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
