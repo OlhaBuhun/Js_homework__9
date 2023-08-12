98291142a980393ecdd054a3fce5abea2a9fac20
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-12 15:31:04 +0000

    Deploying to gh-pages from @ OlhaBuhun/Js_homework__9@90746aae0f859fd282e142fffbcee7446726aac5 🚀

---
+++ 01-color-switcher.1290ad92.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){console.log(t.currentTarget),body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),o.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.1290ad92.js.map
