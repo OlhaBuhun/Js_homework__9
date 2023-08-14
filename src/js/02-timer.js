9411e0becd577e2d6a49a237ce554b419e47efb1
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-14 20:22:53 +0300

    Setting the timer in the second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,6 +15,10 @@ const refs = {
 }
 
 // console.log(dataDay);
+refs.buttonStart.addEventListener('click', onStartTimer);
+refs.input.addEventListener('click', onInput);
+
+
 
 const options = {
   enableTime: true,
@@ -22,11 +26,61 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0].getTime());
+    const selectedDate = selectedDates[0].getTime();
+    console.log(selectedDate);
+    const currentData = Date.now();
+    console.log(currentData);
+
+    if(selectedDate < currentData){
+      alert ("Please choose a date in the future") ;
+    } else {
+      const timerId = setInterval(() => {
+        const currentTime = Date.now();
+            const deltaTime =  selectedDate - currentTime;
+            const { days, hours, minutes, seconds } = convertMs(deltaTime)
+            console.log(`${days}:${hours}:${minutes}:${seconds}`);
+           }, 1000)
+      
+        
+    }
+    
   },
+  
 };
 
-const currentTime = new Date();
-console.log(currentTime);
+
 
 flatpickr(refs.input, options);
+console.dir(options.selectedDate);
+
+function onInput(evt) {
+
+  console.log(evt.target);
+  
+  
+
+}
+
+
+function onStartTimer() {
+  const currentData = Data.now();
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
